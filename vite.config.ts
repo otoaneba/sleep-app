ec8ba966b2cb3e17fd8c3d07af654fb5114a2f4c
Naoto Abe <naotoabe1@gmail.com>
2025-03-23 10:54:29 +0100

    update vite confit for environment url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/pages/nabe7/sleep-app/' : '/'
+  base: process.env.NODE_ENV === 'production' ? '/pages/nabe7/sleep-app/' : '/',
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(
+      process.env.MODE === 'production'
+        ? 'https://your-fly-app.fly.dev/api'
+        : 'http://localhost:3001/api'
+    )
+  }
 })
